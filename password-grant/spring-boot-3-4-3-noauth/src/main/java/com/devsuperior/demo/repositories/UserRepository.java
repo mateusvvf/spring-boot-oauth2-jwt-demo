2cc2313bd347a4e06976187a8b7fd37a4dadc69a
Mateus Viana <mateusvvf@gmail.com>
2025-10-29 10:54:02 -0300

    Spring Security checklist, not tested

---
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package com.devsuperior.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.projections.UserDetailsProjection;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+	
+	@Query(nativeQuery = true, value = "SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority "
+			+ "FROM tb_user JOIN tb_user_role ON tb_user.id = tb_user_role.user_id "
+			+ "JOIN tb_role ON tb_role.id = tb_role.id = tb_user_role.role_id "
+			+ "WHERE tb_user.email = :email")
+	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+}
