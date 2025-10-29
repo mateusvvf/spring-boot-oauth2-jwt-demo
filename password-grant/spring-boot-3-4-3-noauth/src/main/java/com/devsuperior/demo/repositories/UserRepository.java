619bb3ac47c9afbfa258f9834e770603ead769cb
Mateus Viana <mateusvvf@gmail.com>
2025-10-29 15:52:38 -0300

    Access control by role, by route

--- password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	@Query(nativeQuery = true, value = "SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority "
 			+ "FROM tb_user JOIN tb_user_role ON tb_user.id = tb_user_role.user_id "
-			+ "JOIN tb_role ON tb_role.id = tb_role.id = tb_user_role.role_id "
+			+ "JOIN tb_role ON tb_role.id = tb_user_role.role_id "
 			+ "WHERE tb_user.email = :email")
 	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 
